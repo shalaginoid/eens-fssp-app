cf7e8aa1af0243b46a39fe6a679eca6753b05722
shalaginoid <shalaginoid@mail.ru>
2025-09-04 16:04:06 +0500

    commit

---
+++ db.js
@@ -0,0 +1,4 @@
+export default {
+  connectString:
+    'Data Source=s-eens-mss-sql4;Database=EensGosuslugiNotifications;User Id=gosuslugi;Password=123!@#qwe;TrustServerCertificate=true; Request Timeout=60000',
+};
