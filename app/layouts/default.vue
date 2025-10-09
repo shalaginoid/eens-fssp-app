b6843fbb9f527a973fdf9b5786bf4eaebf9e95db
shalaginoid <shalaginoid@mail.ru>
2025-10-09 10:38:16 +0500

    commit

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -35,28 +35,6 @@
 </template>
 
 <script setup lang="ts">
-import type { DropdownMenuItem } from '@nuxt/ui';
-import UserMenuClient from '~/components/UserMenu.client.vue';
-
-const { loggedIn, user, clear: clearSession } = useUserSession();
-
-const username = computed(() => {
-  if (loggedIn.value && user.value?.fullname) {
-    return user.value.fullname
-      .split(/\s+/)
-      .map((w: string, i: any) =>
-        i ? w.substring(0, 1).toUpperCase() + '.' : w,
-      )
-      .join(' ');
-  }
-});
-
-const avatar = computed(() => {
-  if (loggedIn.value && user.value?.fullname) {
-    return `http://portal.eksbyt.ru/docum/DocLib1/${user.value.fullname}.jpg`;
-  }
-});
-
 const mainMenu = computed(() => [
   {
     label: 'Уведомления',
@@ -71,37 +49,4 @@ const mainMenu = computed(() => [
     to: '/statistic',
   },
 ]);
-
-const dropdownMenu = ref<DropdownMenuItem[][]>([
-  [
-    {
-      label: username.value,
-      icon: 'i-lucide-user-round',
-      type: 'link',
-      to: '/profile',
-    },
-  ],
-  [
-    {
-      label: 'Пользователи',
-      icon: 'i-lucide-users-round',
-      to: '/users',
-    },
-    {
-      label: 'Сессии',
-      icon: 'i-lucide-monitor-dot',
-      to: '/visitors',
-    },
-    {
-      label: 'Выйти',
-      icon: 'i-lucide-power',
-      onSelect: logout,
-    },
-  ],
-]);
-
-async function logout() {
-  await clearSession();
-  await navigateTo('/login');
-}
 </script>
