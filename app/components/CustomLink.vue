8288abcae10d2c3778a9135ab1b7d0f965c22be0
shalaginoid <shalaginoid@mail.ru>
2025-11-14 09:59:17 +0500

    commit

--- app/components/CustomLink.vue
+++ app/components/CustomLink.vue
@@ -18,11 +18,3 @@ defineProps<{
   target?: '_blank' | '_parent' | '_self' | '_top' | (string & {}) | null;
 }>();
 </script>
-
-<style>
-@reference "@/assets/css/main.css";
-
-.custom-link span {
-  @apply text-secondary border-secondary/30 group-hover:text-secondary/70 group-hover:border-b-secondary/15 border-b transition-colors;
-}
-</style>
