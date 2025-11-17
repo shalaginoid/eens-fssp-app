cec297bf55ca3d491c8367dc5df890cc63a6d23c
shalaginoid <shalaginoid@mail.ru>
2025-11-17 14:22:50 +0500

    commit

--- app/pages/about.vue
+++ app/pages/about.vue
@@ -5,22 +5,19 @@
     </template>
 
     <template #body>
-      <div
-        class="mx-auto flex w-full flex-col gap-4 sm:gap-6 lg:max-w-2xl lg:gap-12"
-      >
-        <div class="space-y-2">
-          <h2 class="font-bold">Стек технологий:</h2>
+      <div class="mx-auto flex w-full flex-col space-y-4 lg:max-w-xl">
+        <h2 class="font-bold">Стек технологий:</h2>
 
-          <ul class="mb-5 list-disc px-5">
-            <li>NodeJS</li>
-            <li>Nuxt</li>
-            <li>Tailwind CSS</li>
-            <li>MSSQL</li>
-          </ul>
+        <ul class="mb-4 list-disc px-5">
+          <li>NodeJS</li>
+          <li>Nuxt</li>
+          <li>Typescript</li>
+          <li>Tailwind CSS</li>
+          <li>MSSQL</li>
+        </ul>
 
-          <h2 class="font-bold">Разработчик:</h2>
-          <p>Отдел сопровождения информационных систем (Шалагинов К. А.)</p>
-        </div>
+        <h2 class="font-bold">Разработчик:</h2>
+        <p>Отдел сопровождения информационных систем (Шалагинов К. А.)</p>
       </div>
     </template>
   </UDashboardPanel>
