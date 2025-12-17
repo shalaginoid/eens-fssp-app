2ad9391903b50a294cd85e4f18bc3d8f0fa2425f
shalaginoid <shalaginoid@mail.ru>
2025-12-17 13:41:19 +0500

    commit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -21,6 +21,12 @@ export default defineNuxtConfig({
           href: 'https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap',
         },
       ],
+      script: [
+        {
+          nomodule: true,
+          innerHTML: `window.location.href = '/${pkg.name}/browser-not-supported.html'`,
+        },
+      ],
     },
   },
   modules: ['@nuxt/ui'],
