0bec8b7816e4312f6728bc2e3c1bd0fe24adb034
shalaginoid <shalaginoid@mail.ru>
2026-04-08 15:09:56 +0500

    commit

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,5 @@
 <template>
-  <UApp>
+  <UApp :locale="ru">
     <NuxtLoadingIndicator color="#f78e1e" />
 
     <NuxtLayout>
@@ -8,38 +8,12 @@
   </UApp>
 </template>
 
-<script setup lang="ts">
+<script lang="ts" setup>
+import { ru } from '@nuxt/ui/locale';
+
 onMounted(() => {
   document.fonts.ready.then(() => {
     document.documentElement.classList.add('fonts-loaded');
   });
 });
 </script>
-
-<style>
-html::before {
-  content: '';
-  display: inline-block;
-  position: fixed;
-  left: 50%;
-  top: 50%;
-  transform: translate(-50%, -50%);
-  width: 36px;
-  height: 36px;
-  background-image: url('/images/loading.gif');
-  background-size: cover;
-  background-repeat: no-repeat;
-}
-
-body {
-  opacity: 0;
-}
-
-.fonts-loaded body {
-  opacity: 1;
-}
-
-.fonts-loaded::before {
-  display: none;
-}
-</style>
--- app/error.vue
+++ app/error.vue
@@ -1,22 +1,29 @@
 <template>
-  <UError
-    :ui="{ statusCode: 'text-[180px] font-black text-secondary/75' }"
-    :error="{
-      statusCode: error?.statusCode,
-      message: error?.message,
-    }"
-    :clear="{
-      color: 'primary',
-      class: 'rounded-full px-8 py-4',
-      label: 'Перейти на главную',
-    }"
-  />
+  <div class="flex h-screen items-center justify-center">
+    <div class="space-y-4 text-center">
+      <h1 class="text-secondary text-9xl font-black">{{ error.status }}</h1>
+
+      <template v-if="error.status === 404">
+        <h2>Страница не найдена</h2>
+      </template>
+
+      <template v-else>
+        <pre>{{ error.message }}</pre>
+      </template>
+
+      <UButton label="На главную" to="/" variant="subtle" color="neutral" />
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
 import type { NuxtError } from '#app';
 
-const props = defineProps({
-  error: Object as () => NuxtError,
+const props = defineProps<{ error: NuxtError }>();
+
+onMounted(() => {
+  document.fonts.ready.then(() => {
+    document.documentElement.classList.add('fonts-loaded');
+  });
 });
 </script>
